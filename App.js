cf0e8e6d588d6fba77c2a982793fcc0cf6861262
ShamAhmad2022 <shamahmadaljalam@gmail.com>
2023-10-02 19:09:55 +0000

    lab42 is done

--- App.js
+++ App.js
@@ -1,11 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Image, StyleSheet, Text, View } from "react-native";
+import UserInput from "./components/UserInput";
+import Results from "./components/Results";
+import { useState } from "react";
+import axios from "axios";
 
 export default function App() {
+  const [weatherData, setWeatherData] = useState(null);
+
+  // Function to fetch weather data from MetaWeather API
+  const fetchWeatherData = async (city) => {
+    try {
+      const apiKey = "d3a99caf79ca4f8ca5695438e081b7ab";
+      const response = await axios.get(
+        `https://api.weatherbit.io/v2.0/current?lat=35.7796&lon=-78.6382&key=${apiKey}&include=minutely&city=${city}`
+      );
+
+      if (response.status === 200) {
+        const weatherData = response.data;
+        setWeatherData(weatherData);
+      } else {
+        setWeatherData(null);
+      }
+    } catch (error) {
+      console.error("Error fetching weather data:", error);
+    }
+  };
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.Text}>Weather APP:</Text>
+      <Image
+        source={require("./assets/weather.png")}
+        style={{ width: 370, height: 100 }}
+      />
+      <UserInput onSearch={fetchWeatherData} />
+      <Results weatherData={weatherData} />
     </View>
   );
 }
@@ -13,8 +42,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#5db5f0",
+  },
+  Text: {
+    fontSize: 33,
+    color: "white",
+    marginBottom: 20,
+    fontWeight: 'bold'
   },
 });
---
+++ README.md
@@ -0,0 +1,18 @@
+# Lab 42
+## Access Control
+Authors: Sham Al-Jalam
+
+#### a weather app using React Native:
+I have built a weather app using React Native and expo that can get weather data about a certain city. i have used weatherbit API, and after you type the city and submit, the city name, weather desc and weather temp will be shown.
+
+to check on my progress through the devlopment of the app i used expo go on my phone and connected to it through a QR code
+
+### Running the app:
+* npx expo start --tunnel
+
+##### Note: if the API call didn't work, try changing the API key to this one: `0fdb048a0bd6490baf9e83648d330124`
+
+### demo
+![WRRC](./assets/weaetherapp1.jpeg)
+![WRRC](./assets/weaetherapp2.jpeg)
+![WRRC](./assets/weaetherapp3.jpeg)
